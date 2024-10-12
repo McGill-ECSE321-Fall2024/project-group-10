f14c82cd9ba4a9b2f32bf88118ebb3f5bcfe3a5d
JudeSousou1 <judesousou@gmail.com>
2024-10-12 13:12:19 -0400

    added game and specific game repository tests

---
+++ GameShop/src/test/java/com/mcgill/ecse321/GameShop/repository/SpecificGameRepositoryTests.java
@@ -0,0 +1,10 @@
+package com.mcgill.ecse321.GameShop.repository;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpecificGameRepositoryTests {
+
+    
+    
+}
