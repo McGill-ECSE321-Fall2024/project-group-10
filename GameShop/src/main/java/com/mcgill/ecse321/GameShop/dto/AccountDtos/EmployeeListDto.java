01705f6cad36117b72363345673f1f5b6f012a4f
JudeSousou1 <judesousou@gmail.com>
2024-11-03 17:45:20 -0500

    all the account endpoints are functioning correctly

---
+++ GameShop/src/main/java/com/mcgill/ecse321/GameShop/dto/AccountDtos/EmployeeListDto.java
@@ -0,0 +1,19 @@
+package com.mcgill.ecse321.GameShop.dto.AccountDtos;
+
+import java.util.List;
+
+public class EmployeeListDto {
+    private List<EmployeeResponseDto> accounts;
+
+   public EmployeeListDto(List<EmployeeResponseDto> accounts){
+        this.accounts = accounts;
+   }
+
+   public List<EmployeeResponseDto> getAccounts(){
+        return accounts;
+   }
+
+   public void setAccounts(List<EmployeeResponseDto> accounts){
+        this.accounts = accounts;
+   }
+}
