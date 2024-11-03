526e13a0fafe77eea3ae52af029c66d8d736de6c
marcgermanos <marc.germanos@mail.mcgill.ca>
2024-11-03 10:43:48 -0500

    Finished wishList Service class

--- GameShop/src/main/java/com/mcgill/ecse321/GameShop/service/WishlistService.java
+++ GameShop/src/main/java/com/mcgill/ecse321/GameShop/service/WishlistService.java
@@ -1,10 +1,12 @@
 package com.mcgill.ecse321.GameShop.service;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import com.mcgill.ecse321.GameShop.exception.GameShopException;
+import com.mcgill.ecse321.GameShop.model.Game;
 import com.mcgill.ecse321.GameShop.model.WishList;
 import com.mcgill.ecse321.GameShop.repository.WishListRepository;
 
@@ -15,13 +17,56 @@ public class WishlistService {
     @Autowired
     private WishListRepository wishListRepository;
 
-    @Transactional
     public WishList findWishlistById(int id) {
         WishList wishList = wishListRepository.findById(id);
         if (wishList == null) {
             throw new GameShopException(HttpStatus.NOT_FOUND,
-                    String.format("There is no evend with ID %d.",id));
+                    String.format("There is no WishList with Id %d.",id));
         }
         return wishList;
+    }
+    @Transactional
+    public WishList addGameToWishlist(int wishlistId, int gameId) {
+        WishList wishList = findWishlistById(wishlistId);
+        if (wishList == null) {
+            throw new GameShopException(HttpStatus.NOT_FOUND,
+                    String.format("There is no WishList with Id %d.",wishlistId));
+        }
+        List<Game> games = wishList.getGames();
+        for (Game game : games) {
+            if (game.getGame_id() == gameId) {
+                //Game already in wishlist
+                return wishList;
+            }
+        }
+        Game game = Game.getWithGame_id(gameId);
+        wishList.addGame(game);
+        return wishListRepository.save(wishList);
+    }
+    @Transactional
+    public WishList removeGameFromWishlist(int wishlistId, int gameId) {
+        WishList wishList = findWishlistById(wishlistId);
+        if (wishList == null) {
+            throw new GameShopException(HttpStatus.NOT_FOUND,
+                    String.format("There is no WishList with Id %d.",wishlistId));
+        }
+        List<Game> games = wishList.getGames();
+        for (Game game : games) {
+            if (game.getGame_id() == gameId) {
+                wishList.removeGame(game);
+                return wishListRepository.save(wishList);
+            }
+        }
+        throw new GameShopException(HttpStatus.NOT_FOUND,
+                String.format("There is no Game with Id %d in the WishList with Id %d.",gameId,wishlistId));
+    }
+
+    public int getWishlistSize(int wishlistId) {
+        WishList wishList = findWishlistById(wishlistId);
+        if (wishList == null) {
+            throw new GameShopException(HttpStatus.NOT_FOUND,
+                    String.format("There is no WishList with Id %d.",wishlistId));
+        }
+        return wishList.getGames().size();
     }
 }
