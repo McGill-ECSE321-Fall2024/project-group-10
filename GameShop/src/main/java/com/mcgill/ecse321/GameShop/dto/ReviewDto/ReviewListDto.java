ac106acca8b5f6e05c6a694e8a609f3e0a959d2a
moh-abdelhady04 <moh.abdelhady04@gmail.com>
2024-11-03 11:41:00 -0500

    Review DTOs

--- GameShop/src/main/java/com/mcgill/ecse321/GameShop/dto/ReviewDto/ReviewListDto.java
+++ GameShop/src/main/java/com/mcgill/ecse321/GameShop/dto/ReviewDto/ReviewListDto.java
@@ -1,5 +1,22 @@
 package com.mcgill.ecse321.GameShop.dto.ReviewDto;
 
+import java.util.List;
+
 public class ReviewListDto {
-    
+
+    private List<ReviewSummaryDto> reviews;
+
+    public ReviewListDto(List<ReviewSummaryDto> reviews) {
+        this.reviews = reviews;
+    }
+
+    // Getters and Setters
+
+    public List<ReviewSummaryDto> getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(List<ReviewSummaryDto> reviews) {
+        this.reviews = reviews;
+    }
 }
