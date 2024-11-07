d90a4f7bfb3d1c0f6e02a7231af8300dc1c1ed1c
anthonysaber <anthony.saber@mail.mcgill.ca>
2024-11-07 14:33:40 -0500

    version 2 it builds

--- GameShop/src/test/java/com/mcgill/ecse321/GameShop/service/GameServiceTests.java
+++ GameShop/src/test/java/com/mcgill/ecse321/GameShop/service/GameServiceTests.java
@@ -42,8 +42,9 @@ public class  GameServiceTests {
     @InjectMocks
     private GameService gameService;
 
-    private static int VALID_GAME_ID = 77;
-    private static int INVALID_GAME_ID = -1;
+    private static int VALID_GAME_ID;
+    private static int INVALID_GAME_ID = 799;
+    private static int INVALID_GAME_ID2 = -1;
     private static String VALID_TITLE = "Valid Title";
     private static String VALID_DESCRIPTION = "Valid Description";
     private static int VALID_PRICE = 100;
@@ -54,6 +55,7 @@ public class  GameServiceTests {
     private static String VALID_MANAGER_EMAIL = VALID_MANAGER.getEmail();
     @Test
     public void testCreateGame() {
+        VALID_GAME_ID = 77;
         // when(managerRepository.findByEmail(VALID_MANAGER_EMAIL)).thenReturn(VALID_MANAGER);
 
 
@@ -64,13 +66,13 @@ public class  GameServiceTests {
         });
         Game createdGame = gameService.createGame(VALID_TITLE, VALID_DESCRIPTION, VALID_PRICE, VALID_GAME_STATUS, VALID_STOCK_QUANTITY, VALID_PHOTO_URL);
         assertNotNull(createdGame);
-        assertEquals(VALID_GAME_ID, createdGame.getGame_id());
-        assertEquals(VALID_TITLE, createdGame.getTitle());
-        assertEquals(VALID_DESCRIPTION, createdGame.getDescription());
-        assertEquals(VALID_PRICE, createdGame.getPrice());
-        assertEquals(VALID_GAME_STATUS, createdGame.getGameStatus());
-        assertEquals(VALID_STOCK_QUANTITY, createdGame.getStockQuantity());
-        assertEquals(VALID_PHOTO_URL, createdGame.getPhotoUrl());
+        assertEquals(VALID_GAME_ID, createdGame.getGame_id(),"Game ID is not the same");
+        assertEquals(VALID_TITLE, createdGame.getTitle(),"Title is not the same");
+        assertEquals(VALID_DESCRIPTION, createdGame.getDescription(),"Description is not the same");
+        assertEquals(VALID_PRICE, createdGame.getPrice(),"Price is not the same");
+        assertEquals(VALID_GAME_STATUS, createdGame.getGameStatus(),"Game status is not the same");
+        assertEquals(VALID_STOCK_QUANTITY, createdGame.getStockQuantity(),"Stock quantity is not the same");
+        assertEquals(VALID_PHOTO_URL, createdGame.getPhotoUrl(),"Photo URL is not the same");
         verify(gameRepository, times(1)).save(any(Game.class));}
     
     @Test
@@ -167,6 +169,88 @@ public class  GameServiceTests {
         verify(gameRepository, times(0)).save(any(Game.class));
     }
 
+    @Test
+    public void testFindGameById() {
+        VALID_GAME_ID = 2;
+        Game createdGame = gameService.createGame(VALID_TITLE, VALID_DESCRIPTION, VALID_PRICE, VALID_GAME_STATUS, VALID_STOCK_QUANTITY, VALID_PHOTO_URL);
+        createdGame.setGame_id(VALID_GAME_ID);
+        when(gameRepository.findById(VALID_GAME_ID)).thenReturn(createdGame);
+
+        Game foundGame = gameService.findGameById(VALID_GAME_ID);
+        assertNotNull(foundGame);
+        assertEquals(VALID_GAME_ID, foundGame.getGame_id());
+        assertEquals(VALID_TITLE, foundGame.getTitle());
+        assertEquals(VALID_DESCRIPTION, foundGame.getDescription());
+        assertEquals(VALID_PRICE, foundGame.getPrice());
+        assertEquals(VALID_GAME_STATUS, foundGame.getGameStatus());
+        assertEquals(VALID_STOCK_QUANTITY, foundGame.getStockQuantity());
+        assertEquals(VALID_PHOTO_URL, foundGame.getPhotoUrl());
+        verify(gameRepository, times(1)).findById(VALID_GAME_ID);
+    }
+    @Test
+    public void testFindGameByInvalidId(){
+        when(gameRepository.findById(INVALID_GAME_ID)).thenReturn(null);
+        GameShopException exception = assertThrows(GameShopException.class, () -> {
+            gameService.findGameById(INVALID_GAME_ID);
+        });
+        assertEquals(HttpStatus.NOT_FOUND, exception.getStatus());
+        assertEquals("Game with ID 799 does not exist", exception.getMessage());
+        verify(gameRepository, times(1)).findById(INVALID_GAME_ID);
+    }
+    @Test
+    public void testFindGameByInvalidIdValue(){
+        
+        GameShopException exception = assertThrows(GameShopException.class, () -> {
+            gameService.findGameById(INVALID_GAME_ID2);
+        });
+        assertEquals(HttpStatus.BAD_REQUEST, exception.getStatus());
+        assertEquals("Game ID must be greater than 0", exception.getMessage());
+        }
+    
+
+    @Test
+    public void testGetAllGames() {
+        // Arrange
+        Game game1 = new Game("Game Title 1", "Description 1", 30, Game.GameStatus.InStock, 100, "http://example.com/image1.jpg");
+        game1.setGame_id(101);
+        Game game2 = new Game("Game Title 2", "Description 2", 50, Game.GameStatus.OutOfStock, 200, "http://example.com/image2.jpg");
+        game2.setGame_id(202);
+        
+        List<Game> gamesArray = Arrays.asList(game1, game2);
+        when(gameRepository.findAll()).thenReturn(gamesArray);
+    
+        // Act
+        Iterable<Game> games = gameService.getAllGames();
+    
+        // Assert
+        assertNotNull(games);
+        List<Game> gameList = new ArrayList<>();
+        games.forEach(gameList::add);
+        assertTrue(gameList.contains(game1));
+        assertTrue(gameList.contains(game2));
+        assertEquals(2, gameList.size());
+    
+        // Assertions for Game 1
+        assertEquals(101, gameList.get(0).getGame_id());
+        assertEquals("Game Title 1", gameList.get(0).getTitle());
+        assertEquals("Description 1", gameList.get(0).getDescription());
+        assertEquals(30, gameList.get(0).getPrice());
+        assertEquals(Game.GameStatus.InStock, gameList.get(0).getGameStatus());
+        assertEquals(100, gameList.get(0).getStockQuantity());
+        assertEquals("http://example.com/image1.jpg", gameList.get(0).getPhotoUrl());
+    
+        // Assertions for Game 2
+        assertEquals(202, gameList.get(1).getGame_id());
+        assertEquals("Game Title 2", gameList.get(1).getTitle());
+        assertEquals("Description 2", gameList.get(1).getDescription());
+        assertEquals(50, gameList.get(1).getPrice());
+        assertEquals(Game.GameStatus.OutOfStock, gameList.get(1).getGameStatus());
+        assertEquals(200, gameList.get(1).getStockQuantity());
+        assertEquals("http://example.com/image2.jpg", gameList.get(1).getPhotoUrl());
+    
+        verify(gameRepository, times(1)).findAll();
+    }
+    
 
 
 
