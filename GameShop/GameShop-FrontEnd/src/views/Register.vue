8cae54632aaa06ae0b71476e2d39aeb89a6e1d70
JudeSousou1 <judesousou@gmail.com>
2024-12-04 00:20:07 -0500

    fixed rules for phone number

--- GameShop/GameShop-FrontEnd/src/views/Register.vue
+++ GameShop/GameShop-FrontEnd/src/views/Register.vue
@@ -23,7 +23,7 @@
       <v-text-field
         v-model="accountData.phoneNumber"
         label="Phone Number"
-        :rules="[rules.required]"
+        :rules="[rules.required, rules.phoneNumber]"
         required
       ></v-text-field>
       <v-text-field
@@ -62,6 +62,7 @@ export default defineComponent({
     const rules = {
       required: (value) => !!value || 'Required.',
       email: (value) => /.+@.+\..+/.test(value) || 'Invalid email.',
+      phoneNumber: (value) => /^[0-9]{10}$/.test(value) || 'Phone number must be 10 digits.',
     };
 
     const submitRegister = async () => {
@@ -75,7 +76,7 @@ export default defineComponent({
     } catch (error) {
       // Handle failure
       console.error('Registration failed:', error);
-      alert('Registration failed. Please try again.');
+      alert('Registration failed. Email already exists. Please try again.');
     }
     };
 
